0d883e22e71b8d92982a7ef31b962ff789a8936b
Hue Pham <huepham113@gmail.com>
2020-11-23 13:04:52 -0500

    Learn passing props through App-Comment-UserInfo-Avatar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import Comment from './components/Comment';
 
 class App extends Component {
   render() {
-    return (
-      <div className='app'>
+    const comment = {
+      date: new Date(),
+      text: 'I hope you enjoy learning React!',
+      author: {
+        name: 'Hello Kitty',
+        avatarUrl: 'https://placekitten.com/g/64/64',
+      },
+    };
     
+    return (
+      <div className='App'>
+        <Comment
+          date={comment.date}
+          text={comment.text}
+          author={comment.author}
+        />
       </div>
     );
   }
