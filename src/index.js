45f7d22f765d61191b74aae7fca12eb5b5c91346
Hue Pham <huepham113@gmail.com>
2020-11-23 11:18:07 -0500

    Add tick.js and update index.js to render ticking clock example

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import tick from './components/tick';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <tick /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
