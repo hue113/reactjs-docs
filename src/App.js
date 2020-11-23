d994c7dd292c9472b86b09ce4881136072f10088
Hue Pham <huepham113@gmail.com>
2020-11-23 18:46:14 -0500

    Add Toggle.js; Toggle2.js; LoggingButton.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import LoggingButton from './components/LoggingButton';
+import Toggle from './components/Toggle';
+import Toggle2 from './components/Toggle2';
 
 class App extends Component {
   render() {
     return (
       <div className='app'>
-    
+          <Toggle />
+          <Toggle2 />
+          <LoggingButton />
       </div>
     );
   }
