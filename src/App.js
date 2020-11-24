a511ec994d93b24ef8f6ab918d5571e4e17b6e90
Hue Pham <huepham113@gmail.com>
2020-11-23 21:36:00 -0500

    Add part02 of lesson 07 code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Page from './components/Page';
 
 class App extends Component {
   render() {
     return (
       <div className='app'>
-    
+        <Page />
       </div>
     );
   }
