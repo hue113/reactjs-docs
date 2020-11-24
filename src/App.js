b8ea8a73214f26b315e1f3c161d5be03d2bcd8c8
Hue Pham <huepham113@gmail.com>
2020-11-24 16:16:01 -0500

    Add App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import EssayForm from './components/EssayForm';
+import FlavorForm from './components/FlavorForm';
+import NameForm from './components/NameForm';
 
 class App extends Component {
   render() {
     return (
       <div className='app'>
-    
+          <NameForm />
+          <br />
+          <EssayForm />
+          <br />
+          <FlavorForm />
       </div>
     );
   }
