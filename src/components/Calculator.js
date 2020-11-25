2c2f4368c853080cfd100bf881eac29f353a31b1
Hue Pham <huepham113@gmail.com>
2020-11-24 23:18:50 -0500

    Add part02 code of lesson10

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,36 +1,80 @@
 import React, { Component } from 'react';
-// import BoilingVerdict from './BoilingVerdict';
+import BoilingVerdict from './BoilingVerdict';
 import TemperatureInput from './TemperatureInput';
 
 class Calculator extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {temperature: ''};
+    constructor(props) {
+        super(props);
+        this.state = {
+            temperature: '',
+            scale: 'c'
+        };
 
-    //     this.handleChange = this.handleChange.bind(this);
-    // }
+        // this.handleChange = this.handleChange.bind(this);
+        this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+        this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+    }
+
+    handleCelsiusChange(temperature) {
+        this.setState({scale: 'c', temperature});
+        // Full code: this.setState({scale: 'c', temperature: temperature});
+    }
+    
+    handleFahrenheitChange(temperature) {
+        this.setState({scale: 'f', temperature});
+        // Full code: this.setState({scale: 'f', temperature: temperature});
+    }
 
     // handleChange(e) {
     //     this.setState({temperature: e.target.value});
     // }
 
     render() {
-        // const temperature = this.state.temperature;
+        const scale = this.state.scale;
+        const temperature = this.state.temperature;
+        const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
+        const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
+
+        /* Full code explaination */
+        // let celsius = 0;
+        // let fahrenheit = 0;
+        // (scale === 'f') ? celsius = tryConvert(temperature, toCelsius) : celsius = temperature;
+        // (scale === 'c') ? fahrenheit = tryConvert(temperature, toFahrenheit) : fahrenheit = temperature;
+
         return (
             <div>
-                <TemperatureInput scale="c" />
-                <TemperatureInput scale="f" />
+                <TemperatureInput
+                    scale="c"
+                    temperature={celsius}
+                    onTemperatureChange={this.handleCelsiusChange} />
+                <TemperatureInput
+                    scale="f"
+                    temperature={fahrenheit}
+                    onTemperatureChange={this.handleFahrenheitChange} />
+                <BoilingVerdict
+                    celsius={parseFloat(celsius)} />
             </div>
-            // <fieldset>
-            //     <legend>Enter temperature in Celsius:</legend>
-            //     <input
-            //         value={temperature}
-            //         onChange={this.handleChange} />
-            //     <BoilingVerdict
-            //         celsius={parseFloat(temperature)} />
-            // </fieldset>
         );
     }
+}
+
+// Helper functions
+function toCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+}
+  
+function toFahrenheit(celsius) {
+    return (celsius * 9 / 5) + 32;
+}
+
+function tryConvert(temperature, convert) {
+    const input = parseFloat(temperature);
+    if (Number.isNaN(input)) {
+      return '';
+    }
+    const output = convert(input);
+    const rounded = Math.round(output * 1000) / 1000;
+    return rounded.toString();
 }
 
 export default Calculator;
